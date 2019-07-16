3f18d44c5062be23b6aa3d744c4cdd5b0d94fdf5
Spencer Stream <sprewstream@gmail.com>
2019-07-16 18:04:20 -0500

    Created game script. Added the ability to track the current turn

---
+++ Katie Soccer/Assets/Scripts/GameScript.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using UnityEngine;
+
+public class GameScript : MonoBehaviour
+{
+    public GameObject[] TeamOnePieces;
+    public GameObject[] TeamTwoPieces;
+    public GameObject ball;
+
+    public enum Team { TeamOne = -1, TeamTwo = 1 };
+
+    private GameObject[] allPieces;
+    private Team currentTurn;
+    private bool piecesMoving = false;
+    private bool piecesWereMoving = false;
+
+    void Awake()
+    {
+        int numberOfAllPieces = TeamOnePieces.Length + TeamTwoPieces.Length + 1;
+        allPieces = new GameObject[numberOfAllPieces];
+
+        int iterator = 0;
+        foreach (GameObject piece in TeamOnePieces)
+        {
+            allPieces[iterator] = piece;
+            iterator = iterator + 1;
+        }
+
+        foreach (GameObject piece in TeamTwoPieces)
+        {
+            allPieces[iterator] = piece;
+            iterator = iterator + 1;
+        }
+
+        allPieces[iterator] = ball;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Array values = Enum.GetValues(typeof(Team));
+        int randomIndex = Mathf.FloorToInt(UnityEngine.Random.Range(0f, values.Length));
+        currentTurn = (Team)values.GetValue(randomIndex);
+        Debug.Log(currentTurn);
+        OnNextTurn();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        piecesMoving = !PiecesStoppedMoving(allPieces);
+        if (piecesMoving)
+        {
+            piecesWereMoving = true;
+            DisablePieceInteraction(TeamOnePieces);
+            DisablePieceInteraction(TeamTwoPieces);
+        }
+        
+        if (!piecesMoving && piecesWereMoving)
+        {
+            piecesWereMoving = false;
+            int nextTurn = (int)currentTurn * -1;
+            currentTurn = (Team)nextTurn;
+            OnNextTurn();
+        }
+    }
+
+    private bool PiecesStoppedMoving(GameObject[] pieces)
+    {
+        foreach (GameObject piece in pieces)
+        {
+            PieceMovement pieceMovement = piece.GetComponent<PieceMovement>();
+            if (pieceMovement.IsMoving)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public void EnablePieceInteraction(GameObject[] pieces)
+    {
+        foreach (GameObject piece in pieces)
+        {
+            PieceInteraction pieceInteraction = piece.GetComponent<PieceInteraction>();
+            pieceInteraction.enabled = true;
+        }
+    }
+
+    public void DisablePieceInteraction(GameObject[] pieces)
+    {
+        foreach (GameObject piece in pieces)
+        {
+            PieceInteraction pieceInteraction = piece.GetComponent<PieceInteraction>();
+            pieceInteraction.enabled = false;
+        }
+    }
+
+    public void IlluminatePieces(GameObject[] pieces)
+    {
+        foreach (GameObject piece in pieces)
+        {
+            PieceAnimation pieceAnimation = piece.GetComponent<PieceAnimation>();
+            pieceAnimation.IlluminatePieceLight();
+        }
+    }
+
+    public void DarkenPieces(GameObject[] pieces)
+    {
+        foreach (GameObject piece in pieces)
+        {
+            PieceAnimation pieceAnimation = piece.GetComponent<PieceAnimation>();
+            pieceAnimation.DarkenPieceLight();
+        }
+    }
+
+    public void OnNextTurn()
+    {
+        if (currentTurn.Equals(Team.TeamOne))
+        {
+            DarkenPieces(TeamTwoPieces);
+            DisablePieceInteraction(TeamTwoPieces);
+            IlluminatePieces(TeamOnePieces);
+            EnablePieceInteraction(TeamOnePieces);
+        }
+        else
+        {
+            DarkenPieces(TeamOnePieces);
+            DisablePieceInteraction(TeamOnePieces);
+            IlluminatePieces(TeamTwoPieces);
+            EnablePieceInteraction(TeamTwoPieces);
+        }
+    }
+}

a9fb92cb3086a997bc287f16205602661c1d2762
Spencer Stream <sprewstream@gmail.com>
2019-07-23 17:21:15 -0500

    Reordered colors to match their layout in the game

--- Katie Soccer/Assets/Scripts/PieceColors.cs
+++ Katie Soccer/Assets/Scripts/PieceColors.cs
@@ -3,23 +3,23 @@
 public static class PieceColors
 {
     public static Color Red = new Color(0.80f, 0.00f, 0.00f);
-    public static Color Blue = new Color(0.14f, 0.60f, 0.95f);
-    public static Color Pink = new Color(1.00f, 0.75f, 0.86f);
-    public static Color Purple = new Color(0.47f, 0.22f, 0.65f);
-    public static Color Yellow = new Color(0.94f, 0.89f, 0.22f);
-    public static Color Green = new Color(0.47f, 0.78f, 0.46f);
+    public static Color Solar = new Color(1.00f, 0.39f, 0.41f);
     public static Color Orange = new Color(0.98f, 0.45f, 0.20f);
-    public static Color Black = new Color(0.13f, 0.13f, 0.13f);
-    public static Color LightBlue = new Color(0.60f, 0.80f, 0.99f);
     public static Color Peach = new Color(1.00f, 0.69f, 0.56f);
     public static Color Gold = new Color(0.80f, 0.74f, 0.39f);
-    public static Color White = new Color(0.94f, 0.94f, 0.94f);
-    public static Color Lavender = new Color(0.65f, 0.65f, 0.93f);
+    public static Color Yellow = new Color(0.94f, 0.89f, 0.22f);
+    public static Color Lime = new Color(0.70f, 1.00f, 0.70f);
+    public static Color Green = new Color(0.47f, 0.78f, 0.46f);
     public static Color Teal = new Color(0.40f, 0.73f, 0.80f);
+    public static Color LightBlue = new Color(0.60f, 0.80f, 0.99f);
+    public static Color Blue = new Color(0.14f, 0.60f, 0.95f);
+    public static Color DarkBlue = new Color(0.10f, 0.10f, 0.41f);
+    public static Color Purple = new Color(0.47f, 0.22f, 0.65f);
+    public static Color Lavender = new Color(0.65f, 0.65f, 0.93f);
+    public static Color HotPink = new Color(1.00f, 0.59f, 0.88f);
+    public static Color Pink = new Color(1.00f, 0.75f, 0.86f);
+    public static Color White = new Color(0.94f, 0.94f, 0.94f);
     public static Color Silver = new Color(0.75f, 0.75f, 0.75f);
     public static Color Gray = new Color(0.39f, 0.39f, 0.39f);
-    public static Color Lime = new Color(0.70f, 1.00f, 0.70f);
-    public static Color Solar = new Color(1.00f, 0.39f, 0.41f);
-    public static Color HotPink = new Color(1.00f, 0.59f, 0.88f);
-    public static Color DarkBlue = new Color(0.10f, 0.10f, 0.41f);
+    public static Color Black = new Color(0.13f, 0.13f, 0.13f);
 }

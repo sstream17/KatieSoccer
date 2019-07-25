49183352c882c523b4a2751bf97c27209032cc0f
Spencer Stream <sprewstream@gmail.com>
2019-07-24 23:08:40 -0500

    Updated the teal and dark blue colors to make them look more true to their real colors in game

--- Katie Soccer/Assets/Scripts/PieceColors.cs
+++ Katie Soccer/Assets/Scripts/PieceColors.cs
@@ -10,10 +10,10 @@ public static class PieceColors
     public static Color Yellow = new Color(0.94f, 0.89f, 0.22f);
     public static Color Lime = new Color(0.70f, 1.00f, 0.70f);
     public static Color Green = new Color(0.47f, 0.78f, 0.46f);
-    public static Color Teal = new Color(0.40f, 0.73f, 0.80f);
+    public static Color Teal = new Color(0.13f, 0.71f, 0.55f);
     public static Color LightBlue = new Color(0.60f, 0.80f, 0.99f);
     public static Color Blue = new Color(0.14f, 0.60f, 0.95f);
-    public static Color DarkBlue = new Color(0.10f, 0.10f, 0.41f);
+    public static Color DarkBlue = new Color(0.00f, 0.25f, 0.50f);
     public static Color Purple = new Color(0.47f, 0.22f, 0.65f);
     public static Color Lavender = new Color(0.65f, 0.65f, 0.93f);
     public static Color HotPink = new Color(1.00f, 0.59f, 0.88f);

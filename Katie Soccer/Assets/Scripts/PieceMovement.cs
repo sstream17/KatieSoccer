3d406b06ab050032e91d9cb13e6b6e391daf15d5
Spencer Stream <sprewstream@gmail.com>
2019-07-27 08:25:07 -0500

    Removed unused Start() method

--- Katie Soccer/Assets/Scripts/PieceMovement.cs
+++ Katie Soccer/Assets/Scripts/PieceMovement.cs
@@ -21,12 +21,6 @@ public class PieceMovement : MonoBehaviour
         SetStartingPositions();
     }
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
